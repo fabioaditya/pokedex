9d1d36e7dbda66983c7f1ff18d8e0b5352e5ec08
Aditya Fabio <fabioaditya1995@gmail.com>
2022-04-02 23:59:58 +0700

    test build 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,11 +5,15 @@ import styles from "../styles/Home.module.css";
 export default function Index() {
   return (
     <div>
-      <div className="flex w-screen h-screen bg-red-500 bg-opacity-90">
+      <div className="flex w-screen h-screen bg-red-600 ">
         <div className="grid grid-cols-1 my-[10px] mx-[10px] px-5 w-screen h-[50px]">
-          <div>component header</div>
-          <div className="bg-white ">test</div>
-          <button className="bg-green-500 w-[100px] rounded-full ">test</button>
+          <div>component header, bulet2 </div>
+          <div className="bg-white h-[500px] ">
+            test diisi sambutan, welcome, carousel
+          </div>
+          <button className="bg-green-500 px-[100px] w-75 rounded-full ">
+            test
+          </button>
           <button className="bg-black text-white w-[100px] rounded-full ">
             test
           </button>
