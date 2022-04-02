40daa0d619cb34a77a51ae1a2651ebc0f9b18183
Aditya Fabio <fabioaditya1995@gmail.com>
2022-04-02 23:46:07 +0700

    git test build

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Index() {
   return (
-    <div className="bg-color-red-500 ">
-      <div className="text-2xl text-red-500 ">testing</div>
-      INI POKEDEX
+    <div>
+      <div className="flex w-screen h-screen bg-red-500 bg-opacity-90">
+        <div className="grid grid-cols-1 my-[10px] mx-[10px] px-5 w-screen h-[50px]">
+          <div>component header</div>
+          <div className="bg-white ">test</div>
+          <button className="bg-green-500 w-[100px] rounded-full ">test</button>
+          <button className="bg-black text-white w-[100px] rounded-full ">
+            test
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
