4fc786e0e4073a6af82b21383eb30469d481ba26
Aditya Fabio <fabioaditya1995@gmail.com>
2022-04-02 23:24:30 +0700

    add tailwind css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     INI POKEDEX
+    <div className="bg-color-red-500 ">
+      <div className="text-2xl text-red-500 ">testing</div>
+      INI POKEDEX
     </div>
-  )
+  );
 }
