55a2870f05a782c59f9aebc98c5ae4a29d8dbf35
Aditya Fabio <fabioaditya1995@gmail.com>
2022-04-03 23:32:51 +0700

    commit carousel component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Carousel from "../components/Carousel.jsx";
 
 export default function Index() {
   return (
     <div>
-      <div className="flex w-screen h-screen bg-red-600 ">
-        <div className="grid grid-cols-1 my-[10px] mx-[10px] px-5 w-screen h-[50px]">
+      <div className="flex w-screen h-screen lg:h-[900px] bg-red-700 ">
+        <div className="grid grid-cols-1 my-[10px] lg:mx-[100px] mx-[35px] w-screen h-[50px]">
           <div>component header, bulet2 </div>
-          <div className="bg-white h-[500px] ">
-            test diisi sambutan, welcome, carousel
+          <div className="bg-white h-auto max-w-[screen] ">
+            <Carousel />
           </div>
+
           <button className="bg-green-500 px-[100px] w-75 rounded-full ">
             test
           </button>
